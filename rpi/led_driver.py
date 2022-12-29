47d56e28bb14269605799dd57d63bbb2d44d6173
Herman Pineda <hermanpineda94@gmail.com>
2022-12-29 01:27:49 -0600

    add tlc5947 led driver implementation for live led tracking

--- rpi/led_driver.py
+++ rpi/led_driver.py
@@ -1,9 +1,19 @@
 from dataclasses import dataclass, field
 from typing import List, Dict
+import board
+import busio
+import digitalio
 import led_mappings
 
-# https://docs.python.org/3.7/library/dataclasses.html#dataclasses.dataclass
+import adafruit_tlc5947
 
+# Initialize SPI bus
+spi = busio.SPI(clock=board.SCLK, MOSI=board.MOSI)
+
+# Initialize TLC5947
+DRIVER_COUNT = 8  # change this to the number of drivers you have chained
+LATCH = digitalio.DigitalInOut(board.D5)
+tlc5947 = adafruit_tlc5947.TLC5947(spi, LATCH, num_drivers=DRIVER_COUNT)
 
 @dataclass
 class LedDriver:
@@ -12,14 +22,14 @@ class LedDriver:
     station_to_channel: Dict[str, int]
 
     def turn_on_LED(self, channel: int):
-        # Calculate offset using driver.id and channel number
         # Set channel PWM to max 4095
-        pwm = 4095
+        channel_index = self.id * 24 + int(channel)
+        tlc5947[channel_index] = 4095
 
     def turn_off_LED(self, channel: int):
-        # Calculate offset using driver.id and channel number
         # Set channel PWM to min 0
-        pwm = 0
+        channel_index = self.id * 24 + int(channel)
+        tlc5947[channel_index] = 0
 
 
 @dataclass
@@ -36,6 +46,16 @@ class MainDriversController:
             LedDriver(7, "Board 7", led_mappings.led_driver_7_channels),
         ]
     )
+    last_processed_station_ids: List[str] = field(default_factory=lambda: [])
+
+    def remove_old_trains(self, new_station_ids):
+        to_be_removed = set(self.last_processed_station_ids).difference(set(new_station_ids))
+        print("Old:", to_be_removed)
+        for station_id in to_be_removed:
+            for driver in self.drivers:
+                if station_id in driver.station_to_channel:
+                    driver.turn_off_LED(driver.station_to_channel[station_id])
+        self.last_processed_station_ids = new_station_ids
 
     def get_station_ids(self, approaching_trains):
         stations = []
@@ -48,6 +68,7 @@ class MainDriversController:
 
     def process_approaching_trains(self, approaching_trains):
         station_ids = self.get_station_ids(approaching_trains)
+        self.remove_old_trains(station_ids)
         for station_id in station_ids:
             for driver in self.drivers:
                 if station_id in driver.station_to_channel:
